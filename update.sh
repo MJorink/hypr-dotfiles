97bf0f04c1729d392be6b1d0d5cd548364a97b02
Jorink <maxjorink@gmail.com>
2025-06-17 15:34:51 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,11 +7,19 @@ git lfs install
 git clone https://github.com/MJorink/hypr-dotfiles.git $HOME/hypr-dotfiles-update
 cd $HOME/hypr-dotfiles-update
 
+echo -e "${GREEN}Backing up user configs${NC}"
+sudo cp -r $HOME/.config/hypr/UserConfigs $HOME/.config/UserConfigs-Backup
+
 echo -e "${GREEN}Copying new configs${NC}"
 sudo cp -r .config $HOME
 sudo cp -r .icons $HOME
 sudo cp -r .themes $HOME
 sudo cp -r .zshrc $HOME
+
+echo -e "${GREEN}Restoring UserKeybinds from backup${NC}"
+sudo cp -r $HOME/.config/hypr/UserConfigs-Backup/UserKeybinds.conf $HOME/.config/hypr/UserConfigs
+
+echo -e "${GREEN}Your UserKeybinds should be restored, be sure to compare all your $HOME/.config/hypr/UserConfigs files with the files in $HOME/.config/UserConfigs-Backup and make changes again if you want to.${NC}"
 
 echo -e "${GREEN}Configs updated!${NC}"
 
